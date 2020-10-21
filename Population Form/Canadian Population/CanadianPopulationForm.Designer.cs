92c445779fc33070586190efe47a756974c9a00c
abhisekChowdhury <60119622+abhisekChowdhury@users.noreply.github.com>
2020-10-21 11:47:09 -0700

    Add files via upload

---
+++ Population Form/Canadian Population/CanadianPopulationForm.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Canadian_Population
+{
+    partial class CanadianPopulationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewPopulation = new System.Windows.Forms.DataGridView();
+            this.listBoxAreaName = new System.Windows.Forms.ListBox();
+            this.checkBox2016Population = new System.Windows.Forms.CheckBox();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.textBoxMax = new System.Windows.Forms.TextBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelAreaCount = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelTotalDwellings = new System.Windows.Forms.Label();
+            this.labelTotalLandArea = new System.Windows.Forms.Label();
+            this.labelTotal2016Population = new System.Windows.Forms.Label();
+            this.labelTotal2011Population = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPopulation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewPopulation
+            // 
+            this.dataGridViewPopulation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPopulation.Location = new System.Drawing.Point(48, 40);
+            this.dataGridViewPopulation.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridViewPopulation.Name = "dataGridViewPopulation";
+            this.dataGridViewPopulation.RowHeadersWidth = 62;
+            this.dataGridViewPopulation.RowTemplate.Height = 33;
+            this.dataGridViewPopulation.Size = new System.Drawing.Size(1320, 358);
+            this.dataGridViewPopulation.TabIndex = 0;
+            // 
+            // listBoxAreaName
+            // 
+            this.listBoxAreaName.FormattingEnabled = true;
+            this.listBoxAreaName.ItemHeight = 25;
+            this.listBoxAreaName.Location = new System.Drawing.Point(557, 493);
+            this.listBoxAreaName.Name = "listBoxAreaName";
+            this.listBoxAreaName.Size = new System.Drawing.Size(307, 379);
+            this.listBoxAreaName.TabIndex = 1;
+            // 
+            // checkBox2016Population
+            // 
+            this.checkBox2016Population.AutoSize = true;
+            this.checkBox2016Population.Location = new System.Drawing.Point(1002, 478);
+            this.checkBox2016Population.Name = "checkBox2016Population";
+            this.checkBox2016Population.Size = new System.Drawing.Size(283, 29);
+            this.checkBox2016Population.TabIndex = 2;
+            this.checkBox2016Population.Text = "Filter by 2016 Population";
+            this.checkBox2016Population.UseVisualStyleBackColor = true;
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(1039, 557);
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.Size = new System.Drawing.Size(177, 31);
+            this.textBoxMin.TabIndex = 3;
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(1039, 629);
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.Size = new System.Drawing.Size(177, 31);
+            this.textBoxMax.TabIndex = 4;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(1022, 738);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(175, 53);
+            this.buttonReset.TabIndex = 5;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 478);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(390, 50);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Summary Statistics";
+            // 
+            // labelAreaCount
+            // 
+            this.labelAreaCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelAreaCount.Location = new System.Drawing.Point(280, 532);
+            this.labelAreaCount.Name = "labelAreaCount";
+            this.labelAreaCount.Size = new System.Drawing.Size(178, 32);
+            this.labelAreaCount.TabIndex = 7;
+            this.labelAreaCount.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 539);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(240, 50);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Area Count";
+            // 
+            // labelTotalDwellings
+            // 
+            this.labelTotalDwellings.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelTotalDwellings.Location = new System.Drawing.Point(280, 585);
+            this.labelTotalDwellings.Name = "labelTotalDwellings";
+            this.labelTotalDwellings.Size = new System.Drawing.Size(178, 32);
+            this.labelTotalDwellings.TabIndex = 13;
+            this.labelTotalDwellings.Text = "label8";
+            // 
+            // labelTotalLandArea
+            // 
+            this.labelTotalLandArea.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelTotalLandArea.Location = new System.Drawing.Point(280, 640);
+            this.labelTotalLandArea.Name = "labelTotalLandArea";
+            this.labelTotalLandArea.Size = new System.Drawing.Size(178, 32);
+            this.labelTotalLandArea.TabIndex = 14;
+            this.labelTotalLandArea.Text = "label4";
+            // 
+            // labelTotal2016Population
+            // 
+            this.labelTotal2016Population.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelTotal2016Population.Location = new System.Drawing.Point(280, 697);
+            this.labelTotal2016Population.Name = "labelTotal2016Population";
+            this.labelTotal2016Population.Size = new System.Drawing.Size(178, 32);
+            this.labelTotal2016Population.TabIndex = 15;
+            this.labelTotal2016Population.Text = "label5";
+            // 
+            // labelTotal2011Population
+            // 
+            this.labelTotal2011Population.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelTotal2011Population.Location = new System.Drawing.Point(280, 759);
+            this.labelTotal2011Population.Name = "labelTotal2011Population";
+            this.labelTotal2011Population.Size = new System.Drawing.Size(178, 32);
+            this.labelTotal2011Population.TabIndex = 16;
+            this.labelTotal2011Population.Text = "label6";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 592);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(316, 50);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Total Dwellings";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 656);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(330, 50);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Total Land Area";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 704);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(444, 50);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Total 2016 Population";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 766);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(444, 50);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Total 2011 Population";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(552, 447);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(358, 50);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Filter by Province";
+            // 
+            // CanadianPopulationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1483, 946);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelTotal2011Population);
+            this.Controls.Add(this.labelTotal2016Population);
+            this.Controls.Add(this.labelTotalLandArea);
+            this.Controls.Add(this.labelTotalDwellings);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelAreaCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.textBoxMax);
+            this.Controls.Add(this.textBoxMin);
+            this.Controls.Add(this.checkBox2016Population);
+            this.Controls.Add(this.listBoxAreaName);
+            this.Controls.Add(this.dataGridViewPopulation);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "CanadianPopulationForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPopulation)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewPopulation;
+        private System.Windows.Forms.ListBox listBoxAreaName;
+        private System.Windows.Forms.CheckBox checkBox2016Population;
+        private System.Windows.Forms.TextBox textBoxMin;
+        private System.Windows.Forms.TextBox textBoxMax;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelAreaCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelTotalDwellings;
+        private System.Windows.Forms.Label labelTotalLandArea;
+        private System.Windows.Forms.Label labelTotal2016Population;
+        private System.Windows.Forms.Label labelTotal2011Population;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Population Form/Canadian Population/CanadianPopulationForm.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+using System.Reflection;
+
+namespace Canadian_Population
+{
+    public partial class CanadianPopulationForm : Form
+    {
+
+        //Create lists here for data to be stored
+        List<PopulationArea> allPopulation = new List<PopulationArea>();
+        List<PopulationArea> selectedPopulation = new List<PopulationArea>();
+
+        public CanadianPopulationForm()
+        {
+            InitializeComponent();
+
+            this.Text = "Canadian Population";
+
+            ReadPopulationData("Population by Geographical Area - Canada.xml");
+
+            // set the form autosize to grow to fit the controls
+
+            this.AutoSize = true;
+            this.AutoSizeMode = AutoSizeMode.GrowOnly;
+            this.Text = "Canadian Population";
+
+            // initialize datagridview control with default settings
+
+            InitializeDataGridView();
+
+            // initialize listbox
+            InitializeListBoxAreaName();
+
+            //Resets everything to default
+            ResetControlsToDefault();
+
+            //Event Listeners
+            buttonReset.Click += (s, e) => ResetControlsToDefault();
+            checkBox2016Population.CheckedChanged += DisplayData;
+            textBoxMax.TextChanged += DisplayData;
+            textBoxMin.TextChanged += DisplayData;
+
+        }
+
+        /// <summary>
+        /// Populates Datagridview data and label data based on filters
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DisplayData(object sender = null, EventArgs e = null)
+        {
+            //Checking Filters
+            dataGridViewPopulation.Rows.Clear();
+            int max = 0;
+            int min = 0;
+
+            if (checkBox2016Population.Checked == true)
+            {
+                try
+                {
+                    int.TryParse(textBoxMax.Text, out max);
+                    int.TryParse(textBoxMin.Text, out min);
+                }
+                catch (Exception ex) {
+                    MessageBox.Show("Invalid Min or Max entered in Filter by 2016 Population");
+                }
+            }
+
+            //List of AreaNames
+            List<string> selectedAreaName = listBoxAreaName.SelectedItems.OfType<string>().ToList();
+
+            //Query for filtering data
+            selectedPopulation = allPopulation.Where(s => selectedAreaName.Contains(s.AreaName)
+                                        && CompareWithMaxMin(s.Population2016, max, min)).ToList();
+
+
+            Console.WriteLine(allPopulation.Count());
+
+
+            Console.WriteLine("Here" + selectedPopulation.Count());
+
+            var selectedPopulationCount = selectedPopulation.Count();
+            var totalDwellings = 0;
+            decimal totalLandArea = 0;
+            var total2016Population = 0;
+            var total2011Population = 0;
+
+            //List to populate selected data based on filters
+            foreach (PopulationArea eachPopulationEntry in selectedPopulation)
+            {
+                dataGridViewPopulation.Rows.Add(
+                    eachPopulationEntry.AreaName,
+                    eachPopulationEntry.Province,
+                    eachPopulationEntry.Population2016,
+                    eachPopulationEntry.Population2011,
+                    eachPopulationEntry.NumberOfDwellings,
+                    eachPopulationEntry.LandArea);
+                totalDwellings += eachPopulationEntry.NumberOfDwellings;
+                totalLandArea += eachPopulationEntry.LandArea;
+                total2016Population += eachPopulationEntry.Population2016;
+                total2011Population += eachPopulationEntry.Population2011;
+            }
+
+            //Calculations for labels
+            labelAreaCount.Text = String.Format("{0:n0}", selectedPopulationCount).ToString();
+            labelTotalDwellings.Text = String.Format("{0:n0}", totalDwellings).ToString();
+            labelTotalLandArea.Text = String.Format("{0:n}", totalLandArea).ToString();
+            labelTotal2016Population.Text = String.Format("{0:n0}", total2016Population).ToString();
+            labelTotal2011Population.Text = String.Format("{0:n0}", total2011Population).ToString();
+
+
+        }
+
+        /// <summary>
+        /// Useful method to compare min and max population, was used in the query for filtering
+        /// </summary>
+        /// <param name="actualSize"></param>
+        /// <param name="max"></param>
+        /// <param name="min"></param>
+        /// <returns></returns>
+        public bool CompareWithMaxMin(int actualSize, double max, double min)
+        {
+            return (checkBox2016Population.Checked == true && actualSize < max && actualSize > min) || checkBox2016Population.Checked == false;
+        }
+
+        /// <summary>
+        /// Populating ListBox with AreaNames
+        /// </summary>
+        private void InitializeListBoxAreaName()
+        {
+            listBoxAreaName.Items.Clear();
+
+            listBoxAreaName.SelectionMode = SelectionMode.MultiExtended;
+
+            listBoxAreaName.DataSource = allPopulation
+                .OrderBy(x => x.AreaName)
+                .Select(x => x.AreaName)
+                .Distinct()
+                .ToList();
+        }
+
+        /// <summary>
+        /// Resets everything to the initial state
+        /// </summary>
+        private void ResetControlsToDefault()
+        {
+            listBoxAreaName.SelectedIndexChanged -= DisplayData;
+            for (int i = 0; i < listBoxAreaName.Items.Count; i++)
+            {
+                listBoxAreaName.SetSelected(i, true);
+            }
+
+            checkBox2016Population.Checked = false;
+            textBoxMin.Clear();
+            textBoxMax.Clear();
+
+            DisplayData();
+
+            listBoxAreaName.SelectedIndexChanged += DisplayData;
+
+            dataGridViewPopulation.Rows.Clear();
+
+            var ascendingPopulationList = allPopulation.OrderBy(x => x.AreaName).ToList();
+            foreach (PopulationArea eachPopulationEntry in ascendingPopulationList)
+            {
+                dataGridViewPopulation.Rows.Add(
+                    eachPopulationEntry.AreaName,
+                    eachPopulationEntry.Province,
+                    eachPopulationEntry.Population2016,
+                    eachPopulationEntry.Population2011,
+                    eachPopulationEntry.NumberOfDwellings,
+                    eachPopulationEntry.LandArea);
+
+            }
+        }
+
+        /// <summary>
+        /// Allows users to select the xml file to be read by the program
+        /// </summary>
+        /// <param name="fileName"></param>
+        private void ReadPopulationData(string fileName)
+        {
+            // open the file for reading
+            OpenFileDialog openFileDialogCSV = new OpenFileDialog
+            {
+
+                InitialDirectory = Path.GetFullPath(Application.StartupPath + "\\..\\.."),
+                Filter = "XML files|*.xml"
+            };
+            StreamReader populationFile = new StreamReader(fileName);
+
+
+            // open the filedialog, get a name, and open the file
+            if (openFileDialogCSV.ShowDialog() == DialogResult.OK)
+            {
+                populationFile = File.OpenText(openFileDialogCSV.FileName);
+            }
+            // create the serializer, note use of typeof
+            XmlSerializer populationSerializer =
+                new XmlSerializer(typeof(List<PopulationArea>), new XmlRootAttribute("ArrayOfPopulationArea"));   //NOTE TO SELF: MAKE SURE YOU HAVE THE RIGHT ROOT ATTRIBUTE! You were stuck here for 2 hours!
+
+            // deserialize to the list of Population from file, note use of cast
+
+            allPopulation = populationSerializer.Deserialize(populationFile) as List<PopulationArea>;
+            populationFile.Close();
+            Console.WriteLine($"=> Reading list of cars from {fileName}");
+
+            // display
+
+            foreach (PopulationArea eachPopulationEntry in allPopulation)
+            {
+                Console.WriteLine($"Area Name: {eachPopulationEntry.AreaName}\nProvince: {eachPopulationEntry.Province}\nPopulation 2016: {eachPopulationEntry.Population2016}\nPopulation 2011: {eachPopulationEntry.Population2011}\nNumber of Dwellings: {eachPopulationEntry.NumberOfDwellings}\nLand Area: {eachPopulationEntry.LandArea}");
+            }
+        }
+
+        /// <summary>
+        /// Set up the GridView control with consistent parameters.
+        /// Easier than using the designer.
+        /// </summary>
+        private void InitializeDataGridView()
+        {
+            dataGridViewPopulation.Columns.Clear();  // any columns created by the designer, get rid of them
+            dataGridViewPopulation.ReadOnly = true;  // no cell editing allowed
+            dataGridViewPopulation.AllowUserToAddRows = false;     // no rows can be added or deleted
+            dataGridViewPopulation.AllowUserToDeleteRows = false;
+            dataGridViewPopulation.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewPopulation.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            dataGridViewPopulation.AutoSize = false;        // don't autosize the cells
+            dataGridViewPopulation.RowHeadersVisible = false;
+            dataGridViewPopulation.Width = 900;
+
+            // right justify everything
+
+            dataGridViewPopulation.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridViewPopulation.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+
+            // TASK: add the datagridview columns here
+
+            foreach (PropertyInfo eachPopulationEntry in (new PopulationArea()).GetType().GetProperties())
+            {
+                DataGridViewTextBoxColumn column = new DataGridViewTextBoxColumn()
+                {
+                    Name = eachPopulationEntry.Name,
+                    ValueType = eachPopulationEntry.GetType()
+                };
+
+                dataGridViewPopulation.Columns.Add(column);
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// Class that includes relevant properties for a population area
+    /// </summary>
+    [Serializable]
+    public class PopulationArea
+    {
+        /// <summary>
+        /// Population Area Name
+        /// </summary>
+        public string AreaName { get; set; } 
+        /// <summary>
+        /// Name of Province
+        /// </summary>
+        public string Province { get; set; }
+        /// <summary>
+        /// 2016 population
+        /// </summary>
+        public int Population2016 { get; set; }
+        /// <summary>
+        /// 2011 population
+        /// </summary>
+        public int Population2011 { get; set; }
+        /// <summary>
+        /// Number of dwellings (like houses) in the area
+        /// </summary>
+        public int NumberOfDwellings { get; set; }
+        /// <summary>
+        /// Land area in square kilometers.
+        ///     Used to calculate density.
+        /// </summary>
+        public decimal LandArea { get; set; }
+
+        public PopulationArea() { }
+    }
+}
